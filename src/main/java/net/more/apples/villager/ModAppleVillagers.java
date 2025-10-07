ef2301306fc5ce34574bf5c045d83a4d73371b13
JONE 500 <Ingkawat2023Reals@gmail.com>
2025-10-08 00:47:01 +0700

    fix villagers and th_th.json

--- src/main/java/net/more/apples/villager/ModAppleVillagers.java
+++ src/main/java/net/more/apples/villager/ModAppleVillagers.java
@@ -36,7 +36,7 @@ public class ModAppleVillagers {
                         ImmutableSet.of(), ImmutableSet.of(), SoundEvents.ENTITY_VILLAGER_WORK_FISHERMAN));
                  */
                 new VillagerProfession(Text.translatable("entity.minecraft.villager.apple_master"), entry -> entry.matchesKey(type), entry -> entry.matchesKey(type),
-                        ImmutableSet.of(), ImmutableSet.of(), SoundEvents.ENTITY_VILLAGER_WORK_FARMER));
+                        ImmutableSet.of(), ImmutableSet.of(), SoundEvents.ENTITY_VILLAGER_WORK_FISHERMAN));
     }
     /*
     private static PointOfInterestType registerPoi(String name, Block block) {
