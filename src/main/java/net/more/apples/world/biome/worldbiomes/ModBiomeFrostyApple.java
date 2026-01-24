8853b0e3e5d3304c4e5501719d84eece4baa88da
JONE 500 <Ingkawat2023Reals@gmail.com>
2026-01-24 19:57:38 +0700

    man.....

--- src/main/java/net/more/apples/world/biome/worldbiomes/ModBiomeFrostyApple.java
+++ src/main/java/net/more/apples/world/biome/worldbiomes/ModBiomeFrostyApple.java
@@ -73,7 +73,7 @@ public class ModBiomeFrostyApple {
 //                GenerationStep.Feature.VEGETAL_DECORATION,
 //                VegetationPlacedFeatures.PATCH_GRASS_PLAIN);
 
-        DefaultBiomeFeatures.addLeafLitter(biomeBuilder);
+        //DefaultBiomeFeatures.addLeafLitter(biomeBuilder);
         DefaultBiomeFeatures.addDefaultGrass(biomeBuilder);
 //        biomeBuilder.feature(
 //                GenerationStep.Feature.VEGETAL_DECORATION,
