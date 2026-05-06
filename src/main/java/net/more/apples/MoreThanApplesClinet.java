bbe1266483d9ddbeeaf58aeb7bfc455df825f104
JONE500I007 <Ingkawat2023Reals@gmail.com>
2026-05-06 15:48:48 +0700

    Man..........

---
+++ src/main/java/net/more/apples/MoreThanApplesClinet.java
@@ -0,0 +1,10 @@
+package net.more.apples;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreThanApplesClinet implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/more/apples/world/biome/ModTerraBlenderAPI.java
@@ -0,0 +1,6 @@
+package net.more.apples.world.biome;
+
+import terrablender.api.TerraBlenderApi;
+
+public class ModTerraBlenderAPI implements TerraBlenderApi {
+}
