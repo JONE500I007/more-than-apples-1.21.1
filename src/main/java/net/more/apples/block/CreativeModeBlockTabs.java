d17baa05d163e70617ee19c20ba703bfa576fc73
JONE500I007 <Ingkawat2023Reals@gmail.com>
2026-05-08 11:06:18 +0700

    GUYUYYUQYUYQ

--- src/main/java/net/more/apples/block/CreativeModeBlockTabs.java
+++ src/main/java/net/more/apples/block/CreativeModeBlockTabs.java
@@ -4,8 +4,14 @@ import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.more.apples.MoreThanApples;
 
+import static net.more.apples.block.general_block.ModGeneralBlock.*;
 import static net.more.apples.block.ore_block.ModBlockOre.*;
-import static net.more.apples.block.wood_type.apple_wood.AppleWoodBlocks.APPLE_LOG;
+import static net.more.apples.block.wood_type.apple_wood.AppleWoodBlocks.*;
+import static net.more.apples.block.wood_type.apple_wood.AppleWoodVariants.*;
+import static net.more.apples.block.wood_type.frosty_wood.FrostyAppleWoodBlocks.*;
+import static net.more.apples.block.wood_type.frosty_wood.FrostyAppleWoodVariants.*;
+import static net.more.apples.block.wood_type.test_wood.TestAppleWoodBlocks.*;
+import static net.more.apples.block.wood_type.test_wood.TestAppleWoodVariants.*;
 
 //import static net.more.apples.block.ore_block.ModBlockOre.*;
 
@@ -23,7 +29,71 @@ public class CreativeModeBlockTabs {
         CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.BUILDING_BLOCKS)
                 .register(entrise -> {
                     entrise.accept(APPLE_LOG);
+                    entrise.accept(APPLE_WOOD);
+                    entrise.accept(STRIPPED_APPLE_LOG);
+                    entrise.accept(STRIPPED_APPLE_WOOD);
+                    entrise.accept(APPLE_PLANKS);
+                    entrise.accept(APPLE_STAIRS);
+                    entrise.accept(APPLE_SLAB);
+                    entrise.accept(APPLE_FENCE);
+                    entrise.accept(APPLE_FENCE_GATE);
+                    entrise.accept(APPLE_DOOR);
+                    entrise.accept(APPLE_TRAPDOOR);
+                    entrise.accept(APPLE_PRESSURE_PLATE);
+                    entrise.accept(APPLE_BUTTON);
+
+                    entrise.accept(TEST_APPLE_LOG);
+                    entrise.accept(TEST_APPLE_WOOD);
+                    entrise.accept(STRIPPED_TEST_APPLE_LOG);
+                    entrise.accept(STRIPPED_TEST_APPLE_WOOD);
+                    entrise.accept(TEST_APPLE_PLANKS);
+                    entrise.accept(TEST_APPLE_STAIRS);
+                    entrise.accept(TEST_APPLE_SLAB);
+                    entrise.accept(TEST_APPLE_FENCE);
+                    entrise.accept(TEST_APPLE_FENCE_GATE);
+                    entrise.accept(TEST_APPLE_DOOR);
+                    entrise.accept(TEST_APPLE_TRAPDOOR);
+                    entrise.accept(TEST_APPLE_PRESSURE_PLATE);
+                    entrise.accept(TEST_APPLE_BUTTON);
+
+                    entrise.accept(FROSTY_APPLE_LOG);
+                    entrise.accept(FROSTY_APPLE_WOOD);
+                    entrise.accept(STRIPPED_FROSTY_APPLE_LOG);
+                    entrise.accept(STRIPPED_FROSTY_APPLE_WOOD);
+                    entrise.accept(FROSTY_APPLE_PLANKS);
+                    entrise.accept(FROSTY_APPLE_STAIRS);
+                    entrise.accept(FROSTY_APPLE_SLAB);
+                    entrise.accept(FROSTY_APPLE_FENCE);
+                    entrise.accept(FROSTY_APPLE_FENCE_GATE);
+                    entrise.accept(FROSTY_APPLE_DOOR);
+                    entrise.accept(FROSTY_APPLE_TRAPDOOR);
+                    entrise.accept(FROSTY_APPLE_PRESSURE_PLATE);
+                    entrise.accept(FROSTY_APPLE_BUTTON);
                 });
 
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.NATURAL_BLOCKS)
+                .register(entrise -> {
+                    entrise.accept(APPLE_LOG);
+                    entrise.accept(APPLE_LEAVES);
+                    entrise.accept(FRUIT_APPLE_LEAVES);
+                    entrise.accept(APPLE_SAPLING);
+
+                    entrise.accept(GOLDEN_APPLE_LEAVES);
+                    entrise.accept(FRUIT_GOLDEN_APPLE_LEAVES);
+                    entrise.accept(GOLDEN_APPLE_SAPLING);
+
+                    entrise.accept(TEST_APPLE_LOG);
+                    entrise.accept(TEST_APPLE_LEAVES);
+                    entrise.accept(TEST_APPLE_SAPLING);
+
+                    entrise.accept(FROSTY_APPLE_LOG);
+                    entrise.accept(FROSTY_APPLE_LEAVES);
+                    entrise.accept(FROSTY_APPLE_SAPLING);
+                });
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+                .register(entrise -> {
+                    entrise.accept(APPLE_BARREL);
+                });
     }
 }
--- src/main/java/net/more/apples/item/CreativeModeItemTabs.java
+++ src/main/java/net/more/apples/item/CreativeModeItemTabs.java
@@ -6,6 +6,9 @@ import net.more.apples.MoreThanApples;
 import net.more.apples.item.general_item.ModGeneralItems;
 
 import static net.more.apples.item.apple_item.ModAppleFoodItems.*;
+import static net.more.apples.item.apple_wood_item.AppleWoodItem.*;
+import static net.more.apples.item.apple_wood_item.FrostyAppleWoodItem.*;
+import static net.more.apples.item.apple_wood_item.TestAppleWoodItem.*;
 import static net.more.apples.item.general_item.ModGeneralItems.*;
 
 public class CreativeModeItemTabs {
@@ -24,9 +27,34 @@ public class CreativeModeItemTabs {
                     entrise.accept(NETHERITE_APPLE);
                     entrise.accept(ENCHANTED_NETHERITE_APPLE);
                 });
+
         CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.INGREDIENTS)
                 .register(entrise -> {
                     entrise.accept(DIAMOND_NUGGET);
+                });
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+                .register(entrise -> {
+                    entrise.accept(APPLE_SIGN);
+                    entrise.accept(APPLE_HANGING_SIGN);
+
+                    entrise.accept(TEST_APPLE_SIGN);
+                    entrise.accept(TEST_APPLE_HANGING_SIGN);
+
+                    entrise.accept(FROSTY_APPLE_SIGN);
+                    entrise.accept(FROSTY_APPLE_HANGING_SIGN);
+                });
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES)
+                .register(entrise -> {
+                    entrise.accept(APPLE_BOAT);
+                    entrise.accept(APPLE_CHEST_BOAT);
+
+                    entrise.accept(TEST_APPLE_BOAT);
+                    entrise.accept(TEST_APPLE_CHEST_BOAT);
+
+                    entrise.accept(FROSTY_APPLE_BOAT);
+                    entrise.accept(FROSTY_APPLE_CHEST_BOAT);
                 });
     }
 }
