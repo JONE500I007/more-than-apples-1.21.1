02f9fe3a1c4da37330f44d7a20572cc1e1c602c3
JONE 500 <Ingkawat2023Reals@gmail.com>
2026-09-12 09:36:57 +0700

    fix the server wehern you open apple codex ;3

--- src/main/java/net/more/apples/item/custom/AppleCodexItem.java
+++ src/main/java/net/more/apples/item/custom/AppleCodexItem.java
@@ -1,7 +1,5 @@
 package net.more.apples.item.custom;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -15,11 +13,6 @@ public class AppleCodexItem extends Item {
 
     @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
-
-        if(level.isClientSide()) {
-            Minecraft.getInstance().setScreen(new AppleCodexScreen(Component.literal("Apple Codex")));
-        }
-
         return InteractionResult.SUCCESS;
     }
 }
--- src/main/java/net/more/apples/item/custom/AppleCodexScreen.java
+++ src/main/java/net/more/apples/item/custom/AppleCodexScreen.java
@@ -71,7 +71,7 @@ public class AppleCodexScreen extends Screen {
             new Page7ApplePage(),
             new Page8ApplePage());
 
-    protected AppleCodexScreen(Component title) {
+    public AppleCodexScreen(Component title) {
         super(title);
     }
 
